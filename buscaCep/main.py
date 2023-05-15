717c576f5cb948c0a03f546b918ca94a368e76bb
Kaylaine <85030686+Kaylaineasb@users.noreply.github.com>
2023-05-15 10:31:58 -0300

    adicionando

---
+++ buscaCep/main.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+cep = '49200000'
+cep =cep.replace("-","").replace(".","").replace(" ","")
+
+if(len(cep)==8):
+    link = f'https://viacep.com.br/ws/{cep}/json/'
+    requisicao = requests.get(link)
+
+    print(requisicao)
+    dic_requisicao = requisicao.json()
+
+    uf=dic_requisicao['uf']
+    cidade = dic_requisicao['localidade']
+    bairro = dic_requisicao['bairro']
+    print(uf,cidade,bairro)
+else:
+    print("CEP INVÁLIDO")
